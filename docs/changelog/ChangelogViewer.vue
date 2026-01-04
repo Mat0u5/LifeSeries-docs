2f03168d02d8c93bd8f5e88bb6f6185ab3738710
Mat0u5 <matous.danko@gmail.com>
2026-01-04 16:09:24 +0100

    Perchance

--- docs/changelog/ChangelogViewer.vue
+++ docs/changelog/ChangelogViewer.vue
@@ -91,7 +91,6 @@ function renderMarkdown(markdown) {
       if (isExternal) {
         return `<a href="${url}" target="_blank" rel="noopener noreferrer">${text}</a>`
       } else {
-        // Internal link - no target="_blank"
         return `<a href="${url}">${text}</a>`
       }
     })
@@ -99,27 +98,7 @@ function renderMarkdown(markdown) {
     .replace(/\*(.*?)\*/gim, '<em>$1</em>')
     .replace(/`([^`]+)`/gim, '<code>$1</code>')
     .replace(/^\s*[-*]\s+(.*)$/gim, '<li>$1</li>')
-    .replace(/(<li>.*<\/li>\n?)+/gim, '<ul>function renderMarkdown(markdown) {
-  let html = markdown
-    .replace(/^### (.*$)/gim, '<h3>$1</h3>')
-    .replace(/^## (.*$)/gim, '<h2>$1</h2>')
-    .replace(/^# (.*$)/gim, '<h1>$1</h1>')
-    .replace(/^\s*[-]{3,}\s*$/gim, '<hr>')
-    .replace(/\[([^\]]+)\]\(([^)]+)\)/gim, '<a href="$2" target="_blank" rel="noopener noreferrer">$1</a>')
-    .replace(/\*\*(.*?)\*\*/gim, '<strong>$1</strong>')
-    .replace(/\*(.*?)\*/gim, '<em>$1</em>')
-    .replace(/`([^`]+)`/gim, '<code>$1</code>')
-    .replace(/^\s*[-*]\s+(.*)$/gim, '<li>$1</li>')
     .replace(/(<li>.*<\/li>\n?)+/gim, '<ul>$&</ul>')
-    .replace(/\n{2,}/g, '</p><p>')
-    .replace(/\n/g, '<br>')
-
-  if (!html.startsWith('<p>') && !html.startsWith('<h')) {
-    html = '<p>' + html + '</p>'
-  }
-
-  return html
-}</ul>')
     .replace(/\n{2,}/g, '</p><p>')
     .replace(/\n/g, '<br>')
 
@@ -209,7 +188,7 @@ const filteredChangelogs = computed(() => {
         :key="log.version"
         class="changelog-entry"
       >
-        <h2 id="version-{{ log.version }}">Version {{ log.version }}</h2>
+        <h2 :id="`version-${log.version}`">Version {{ log.version }}</h2>
         <div class="content" v-html="log.content"></div>
       </div>
     </div>
